34df64f3502c3741716822e04dd6775f04c961f4
Qianyi Cheng <qcheng1@log001.cm.cluster>
2019-05-15 18:39:58 -0500

    update gen_irc.py

--- lib/gen_irc.py
+++ lib/gen_irc.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python
 
-from PDB import *
+from read_write_pdb import *
 from rms import *
 from numpy import *
 import sys, re, os
-import glob
+import argparse
 
-def get_gaussian_freq(gfile,num_atoms):
+def get_gaussian_freq(gfile):
     with open(gfile) as f:
         lines = f.readlines()
     p_start = []
     f_lines = []
+    atom_name = []
+    for i in range(len(lines)):
+        if 'NAtoms=' in lines[i]:
+            num_atoms = int(lines[i].split()[1])
+            break
+    for i in range(len(lines)):
+        if 'Charge =' in lines[i]:
+            v = lines[i].split()
+            charge = int(v[2])
+            multip = int(v[5])
+            if "Redundant internal coordinates found in file." in lines[i+1]:
+                for idx in range(i+2,i+2+num_atoms):
+                    if ',' in lines[idx]:
+                        atom_name.append(lines[idx][:-1].split(','))
+                    else:
+                        va = lines[idx][:-1].split()
+                        if len(va) == 4:
+                            atom_name.append([va[0],'0'])
+                        else:
+                            atom_name.append(lines[idx][:-1].split()[:2])
+
     for i in range(len(lines)):
         if 'Standard orientation' in lines[i]:
             p_start.append(i+5)
@@ -23,6 +44,7 @@ def get_gaussian_freq(gfile,num_atoms):
         v = lines[i].split()
         opt.append([float(v[3]),float(v[4]),float(v[5])])
     opt = array(opt)
+    print len(opt)
 
     ### Read in imaginary frequency
     freq_xyz  = {}
@@ -51,13 +73,13 @@ def get_gaussian_freq(gfile,num_atoms):
             for k in range(3):
                 freq_xyz[keys[i*3+k]].append([float(v[3*k+2]),float(v[3*k+3]),float(v[3*k+4])])
 
-    return opt, atom_idx[3], freq_xyz, freq_info        
+    return opt, atom_idx[3], freq_xyz, freq_info, charge, multip, atom_name        
 
 
-def form_irc_xyz(opt,atom_idx,xyz):
+def form_irc_xyz(opt,atom_idx,xyz,scale):
     irc1 = []
     irc2 = []
-    scale = 0.1
+#    scale = 0.1
     for i in range(opt.shape[0]):
         if i not in atom_idx:
             irc1.append(opt[i,:])
@@ -67,39 +89,74 @@ def form_irc_xyz(opt,atom_idx,xyz):
             irc2.append(opt[i,:]-scale*array(xyz[atom_idx.index(i)]))
     return array(irc1), array(irc2)
 
-def write_input(input,dir1,dir2,irc1,irc2):
-    with open(input) as f:
+def write_input(inp_f,dir1,dir2,irc1,irc2,atom_name,charge,multip):
+    with open(inp_f) as f:
         lines = f.readlines()
     f1 = open('%s/1.inp'%dir1,'w')
     f2 = open('%s/1.inp'%dir2,'w')
-    count = 0
+    
     for i in range(len(lines)):
         v = lines[i].split()
-        if len(v) == 5 and '#P' not in lines[i]:
-            f1.write('%6s%6s%12.6f%12.6f%12.6f\n'%(v[0],v[1],irc1[count,0],irc1[count,1],irc1[count,2]))
-            f2.write('%6s%6s%12.6f%12.6f%12.6f\n'%(v[0],v[1],irc2[count,0],irc2[count,1],irc2[count,2]))
-            count += 1
-        else: 
+#        if len(v) == 5 and '#P' not in lines[i] and i > 7:
+        if len(v) != 0 and "#P" in v[0]:
+#            print v
+            for tag in v:
+                if 'opt' in tag: 
+                    v[v.index(tag)] = 'opt'
+                if 'geom' in tag or 'guess' in tag or 'iop' in tag:
+                    v[v.index(tag)] = ''
+            for word in v:
+                f1.write('%s '%word)
+                f2.write('%s '%word)
+            f1.write('\n')
+            f2.write('\n')
+        elif len(v) == 2 and v[0] == str(charge) and v[1] == str(multip):
+            f1.write(lines[i])
+            f2.write(lines[i])
+            for atom in range(len(irc1)):
+                f1.write('%6s%6s%12.6f%12.6f%12.6f\n'%(atom_name[atom][0],atom_name[atom][1],irc1[atom,0],irc1[atom,1],irc1[atom,2]))
+                f2.write('%6s%6s%12.6f%12.6f%12.6f\n'%(atom_name[atom][0],atom_name[atom][1],irc2[atom,0],irc2[atom,1],irc2[atom,2]))
+        elif len(v) == 4 or len(v) == 5:
+            if len(v[-1]) != 1:
+                continue
+            else:
+                f1.write(lines[i])
+                f2.write(lines[i])
+        else:
             f1.write(lines[i])
             f2.write(lines[i])
     f1.close()
     f2.close()
 
-def update_xyz(pdb,xyz):
-    sel_atom = []
-    for i in range(len(pdb)):
-        atom = pdb[i]
-        atom[8:11] = xyz[i,:]
-        sel_atom.append(atom)
-    return sel_atom
-
 
 if __name__ == '__main__':
-    outdir = sys.argv[1]
-    num_atoms=int(sys.argv[2])
-    input = sys.argv[3]
-    opt, atom_idx, freq_xyz, freq_info = get_gaussian_freq(outdir,num_atoms)
+
+    parser = argparse.ArgumentParser(description='Write irc inputs')
+    parser.add_argument('-wdir', dest='output_dir', default=os.path.abspath('./'), help='working dir')
+    parser.add_argument('-dir1', dest='dir1', default=os.path.abspath('./irc1'), help='irc1 dir')
+    parser.add_argument('-dir2', dest='dir2', default=os.path.abspath('./irc2'), help='irc2 dir')
+    parser.add_argument('-outf', dest='gau_out', default=None, help='output_name')
+    parser.add_argument('-inpf', dest='gau_inp', default=None, help='input_name')
+    parser.add_argument('-s', dest='scale', type=float,default=0.1, help='scale_factor')
+
+    args = parser.parse_args()
+    wdir = args.output_dir
+    scale = args.scale
+    dir1 = args.dir1
+    dir2 = args.dir2
+    if args.gau_out is None:
+        out_f = '%s/1.out'%wdir
+    else:
+        out_f = args.gau_out
+
+    if args.gau_inp is None:
+        inp_f = '%s/1.inp'%wdir
+    else:
+        inp_f = args.gau_inp
+
+    opt, atom_idx, freq_xyz, freq_info, charge, multip, atom_name = get_gaussian_freq(out_f)
+#    print charge, multip
 
     xyz = freq_xyz[1]
-    irc1, irc2 = form_irc_xyz(opt,atom_idx,xyz)
-    write_input(input,'irc1','irc2',irc1,irc2)
+    irc1, irc2 = form_irc_xyz(opt,atom_idx,xyz,scale)
+    write_input(inp_f,'irc1','irc2',irc1,irc2,atom_name,charge,multip)
